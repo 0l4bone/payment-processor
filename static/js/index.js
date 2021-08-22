aa04fc5374e17876053f445480fcc602b6733ba9
0l4bone <0l4bone@gmail.com>
2021-08-22 15:27:40 -0500

    of culture in parallel to, not at the expense of, technologY and process

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+function tst(){
+    $("a[id^=view-paste]").click(function() {
+        var uuid = $(this).data("uuid");
+        $.get('/customers/display?paste_uuid=' + uuid, function(data) {
+            $('#display-paste').html(data);
+        });
+    });
+};
